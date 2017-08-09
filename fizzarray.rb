44dd4b40878581785540042f081a11962af1eda0
johnkelley67 <john.kelley@bridgevalley.edu>
2017-08-09 13:47:03 -0400

    it works

--- fizzarray.rb
+++ fizzarray.rb
@@ -1,16 +1,18 @@
 def fizzarray()
-	fizz_array = []
 	x = 0
-	 
+	fizz_array = [] 
 	100.times do 
 		x = x+1
-		if x % 3 == 0
+		if x % 15 == 0
+			fizz_array.push("fizz buzz")
+		elsif x % 5 == 0
+			 fizz_array.push("buzz")
+		elsif x % 3 == 0
 			fizz_array.push("fizz")
-		else 
+		else
 			fizz_array.push(x)	
 		end
 	end
-
 	p fizz_array 	
 end
 	
